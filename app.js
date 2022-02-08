c703eef4d2ccf80aa8d6e15a3425e41dcbf91b33
sukscode <sukritisrivastava02@gmail.com>
2022-02-08 19:11:31 +0530

    saving data to mailchimp server

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import request from "request";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
+import { Http2ServerRequest } from "http2";
+import https from "https";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,10 +24,46 @@ app.post("/", function (req, res) {
   var firstName = req.body.fname;
   var lastName = req.body.lname;
   var email = req.body.email;
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  //console.log(firstName, lastName, email);
+  var jsonData = JSON.stringify(data);
+  const url = "https://us14.api.mailchimp.com/3.0/lists/78cba9deeb";
 
-  console.log(firstName, lastName, email);
+  const options = {
+    method: "POST",
+    auth: "sukriti:2bad108e92ae1b2fb1922dbc1d4db0a7-us14",
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.send("Successfully Subscribed!!");
+    } else {
+      res.send("There was an error with signing up,please try again!");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
   console.log("server is running");
 });
+
+//78cba9deeb list id
+//api key
+//2bad108e92ae1b2fb1922dbc1d4db0a7-us14
