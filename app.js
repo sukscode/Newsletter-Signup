10c234bcb9c263f23e818e46d1196128ff52a07b
sukscode <sukritisrivastava02@gmail.com>
2022-02-08 18:01:01 +0530

    settingUp the Page

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import request from "request";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.htm");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
 
 app.listen(3000, function () {
   console.log("server is running");
